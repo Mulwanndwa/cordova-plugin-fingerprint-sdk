483b87bc6e956a9871c9cfabfc4187b31795f1d3
Mulwanndwa <mulwanndwa.mpho@gmail.com>
2023-01-31 12:15:56 +0200

    Add files via upload

--- www/SimplFingerprintWrapper.js
+++ www/SimplFingerprintWrapper.js
@@ -10,4 +10,8 @@ simpl.OnCheckPermission = function(merchantId="0", phoneNumber="0", email="0", s
     cordova.exec(successCallback, errorCallback, "SimplFingerprintWrapper", "generateFingerprint", [merchantId, phoneNumber, email]);
 };
 
+simpl.openScanner = function(merchantId="0", phoneNumber="0", email="0", successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "SimplFingerprintWrapper", "generateFingerprint", [merchantId, phoneNumber, email]);
+};
+
 module.exports = simpl;
